e1ca44469e433e47e21e82318dd4715c2f0ccc0c
nevrozq <artemker667@gmail.com>
2026-02-14 12:35:33 +0300

    Add dependency: presentation from domain

--- shared/chats/presentation/build.gradle.kts
+++ shared/chats/presentation/build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("presentation-setup")
 }
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.shared.chats.domain)
+        }
+    }
+}
